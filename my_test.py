224921620f8b8721771076592e0caa805cf1ead3
liaoshengchen <liaoshengchen@baidu.com>
2023-03-30 16:19:58 +0800

    work

--- my_test.py
+++ my_test.py
@@ -6,24 +6,24 @@ import matplotlib.pyplot as plt
 import param
 
 metrics = []
+start_predict_idex = 4000
 
-for i in range(0,10000):
+for i in range(0,1000):
     metrics.append(100*math.sin(0.1*i))
 tests = []
-for i in range(0,param.look_back):
+for i in range(start_predict_idex,start_predict_idex+param.look_back):
     tests.append(100*math.sin(0.1*i))
 real = []
-for i in range(0,param.look_back+param.look_forward):
+for i in range(start_predict_idex,start_predict_idex+param.look_back+param.look_forward):
     real.append(100*math.sin(0.1*i))
-train_loader = data_preparation.train_data_prepare(metrics)
+#train_loader = data_preparation.train_data_prepare(metrics)
 
 #net.train(train_loader,"cpu")
 
 
 out = net.predict(tests,"GRU","cpu")
-print(out)
-tests.append(out.item())
-
+print(out.tolist())
+tests.extend(out.tolist()[0])
 
 plt.plot(tests)
 
--- param.py
+++ param.py
@@ -1,10 +1,10 @@
 import torch
 
 look_back = 100
-look_forward = 100
-batch_size = 1000
+look_forward = 60
+batch_size = 100
 n_layers = 2
-epochs = 10
+epochs = 200
 
 STATUS_TRAIN =  1
 STATUS_PREDICT = 2

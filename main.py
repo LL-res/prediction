0beec3d711953c4669b96309a7a968eb470502b1
LSC <78481036+LL-res@users.noreply.github.com>
2023-05-02 16:54:19 +0800

    test

--- main.py
+++ main.py
@@ -11,22 +11,17 @@ import param
 
 class Request(object):
     def __init__(self, d):
-       self.__dict__ = d
+        d = json.loads(d)
+        self.__dict__ = d
         
-        
-class ModelInfo(object):
-    def __init__(self,trained,model_type,metric_type):
-        self.trained = trained
-        self.model_type = model_type
-        self.metric_type = metric_type
-        
-        
+
 class Response(object):
-    def __init__(self,trained,model_type,metric_type,prediction):
+    def __init__(self,trained=None,key=None,prediction=None,loss=None,error=""):
         self.trained = trained
-        self.model_type = model_type
-        self.metric_type = metric_type
+        self.key = key
         self.prediction = prediction
+        self.loss = loss
+        self.error = error
         
         
 def check_status(request):
@@ -38,38 +33,63 @@ def check_status(request):
     return status
 
 def handle(request):
-    rsp = Response(False,None,None,None)
-
+    rsp = Response(False,None,None)
+    # 进行预测，接收到数据处理之后，需要传回响应
+    if hasattr(request,'epochs'):
+        param.epochs = request.epochs
+    if hasattr(request,'n_layers'):
+        param.n_layers = request.n_layers
     if check_status(request) & param.STATUS_PREDICT:
         metrics = []
         for index, val in enumerate(request.predict_history):
-            metrics.append(val.metric)
-        out = net.predict(metrics,request.predict_history[0].type)
+            metrics.append(val)
+        try:
+            out = net.predict(metrics,request.key)
+        except Exception as e:
+            rsp.error = e.__dict__
+            return rsp
         rsp.trained = True
-        rsp.metric_type = request.predict_history[0].type
-        rsp.model_type = param.model_type
+        rsp.key = request.key
         rsp.prediction = out.tolist()[0]
-
+        return rsp
+    # 训练，接收到数据后进行处理，传回响应的地址并不是数据传递过来时的地址
     if check_status(request) & param.STATUS_TRAIN:
         metrics = []
         for index, val in enumerate(request.train_history):
-            metrics.append(val.metric)
+            metrics.append(val)
         train_loader = data_preparation.train_data_prepare(metrics)
-        net.train(train_loader, request.train_history[0].type)
+        client = SocketClient(request.resp_recv_address)
+        try:
+            loss = net.train(train_loader, request.key)
+        except Exception as e:
+            rsp.error = e.__dict__
+            client.send(json.dumps(rsp.__dict__))
+            return
         rsp.trained = True
-        rsp.metric_type = request.predict_history[0].type
-        rsp.model_type = param.model_type
+        rsp.key = request.key
+        rsp.loss = loss
+        data = json.dumps(rsp.__dict__)
+        client.send(data)
 
-    return rsp
 
 
+
+class SocketClient:
+    def __init__(self,address):
+        self.address = address
+        self.client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    def send(self,data):
+        self.client_socket.connect(self.address)
+        self.client_socket.send(data.encode())
+        self.client_socket.close()
 
 class SocketServer:
     def __init__(self):
         # unix domain sockets
         socket_family = socket.AF_UNIX
         socket_type = socket.SOCK_STREAM
-
+        if os.path.exists(param.socket_address):
+            os.remove(param.socket_address)
         self.sock = socket.socket(socket_family, socket_type)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(param.socket_address)
@@ -93,7 +113,8 @@ class SocketServer:
                     break
             print(f"recv data from client '{client_address}': {data.decode()}")
             resp = handle(Request(data.decode().removesuffix("\n")))
-            connection.sendall(json.dumps(resp.__dict__).encode())
+            if resp:
+                connection.sendall(json.dumps(resp.__dict__).encode())
             connection.shutdown(socket.SHUT_WR)
 
     def _signal_handler(self, signum, frame):
